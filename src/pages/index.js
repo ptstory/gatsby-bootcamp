e1f206244eb987ce0f02b3d118730291ef22eee3
pstory <perry.t.story@gmail.com>
2020-02-02 17:49:51 -0500

    init

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hello.</h1>
+            <h2>I'm Perry, a full-stack developer living in beautiful Atlanta.</h2>
+        </div>
+    )
+}
+
+export default IndexPage
