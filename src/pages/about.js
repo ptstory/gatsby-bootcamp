e8721ffd276c2fae16e783c934b18d14cef881d2
pstory <perry.t.story@gmail.com>
2020-02-02 18:54:19 -0500

    lesson 5: page layouts

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { Link } from "gatsby";
-import Header from "../components/header"
-import Footer from "../components/footer"
-
+import Layout from "../components/layout";
 
 
 const AboutPage = () => {
     return (
-        <div>
-            <Header />
+        <Layout>
             <h1>About</h1>
             <p>I'm trying to learn GatsbyJS</p>
             <Link to="/contact">Want to work with me? Reach out.</Link>
-            <Footer />
-        </div>
+        </Layout>
     )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,15 @@
 import React from "react";
-import Header from "../components/header"
-import Footer from "../components/footer"
+import Layout from "../components/layout";
+
 
 const ContactPage = () => {
     return (
-        <div>
-            <Header />
+        <Layout>
             <h1>Contact</h1>
             <p>Shoot me a message me at <a href="mailto: perry.t.story@gmail.com">perry.t.story@gmail.com</a></p>
             <p>I don't use twitter but here's the guy I'm learning Gatsby from: <a href="https://twitter.com/andrew_j_mead" target="_blank" rel="noopener noreferrer">Andrew Mead</a> </p>
-            <Footer />
-        </div>
+        </Layout>
+
     )
 }
 
