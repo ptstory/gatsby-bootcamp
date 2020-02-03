1ed59667bf20682243000bff2e7f4a8904721d36
pstory <perry.t.story@gmail.com>
2020-02-02 19:01:55 -0500

    lesson 6: basic styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,8 @@
 import React from "react";
+
 import Header from "./header";
 import Footer from "./footer";
+import "../styles/index.scss";
 
 const Layout = (props) => {
     return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
-import Header from "../components/header"
-import Footer from "../components/footer"
 
 const IndexPage = () => {
     return (
