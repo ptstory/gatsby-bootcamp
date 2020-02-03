b5e96e10dd0db18017791d62b83474d84d8d6b86
pstory <perry.t.story@gmail.com>
2020-02-02 19:37:49 -0500

    styling with css modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Link } from "gatsby";
 
+import headerStyles from "./header.module.scss";
+
 const Header = () => {
     return (
-        <header>
-            <h1>Gatsby Bootcamp Demo</h1>
+        <header className={headerStyles.header}>
+            <h1>
+                <Link className={headerStyles.title} to="/">
+                    Perry Story
+                </Link>
+            </h1>
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+                <ul className={headerStyles.navList}>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
